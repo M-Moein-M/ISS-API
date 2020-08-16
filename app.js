1b65f98cda4d534997373e104f4c9d3e741fcf92
Moein <mohammadmoein.moradi@gamil.com>
2020-08-16 12:45:28 +0430

    Added map to the program using Leaflet library and we refresh the marker on the page at every longitude and latitude update

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// handling the map
+const mymap = L.map('map').setView([0, 0], 1);
+const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
+
+let satelliteMarker = L.marker([0, 0]).addTo(mymap);
+
+
+// handling the api requests
 let apiURL = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 async function fetchData() {
@@ -7,6 +20,10 @@ async function fetchData() {
         document.getElementById('longitude').innerText = 'longitude: ' + data.longitude;
         document.getElementById('latitude').innerText = 'latitude: ' + data.latitude;
         document.getElementById('velocity').innerText = 'velocity: ' + data.velocity + '\nunits: ' + data.units;
+
+        satelliteMarker.setLatLng([data.longitude, data.latitude]);
+
+
     } catch (error) {
         console.log('Maximum amount of request per second reached!');
     }
@@ -15,3 +32,4 @@ async function fetchData() {
 }
 
 setInterval(fetchData, 1500);
+
